d8657bcdfe1998d934b69cb888f6163db2941df2
Ben Rubin <benrubin@Bens-MacBook-Pro-3.local>
2017-08-06 03:03:03 -0500

    update docs app

--- docs-app/app.js
+++ docs-app/app.js
@@ -1,4 +1,14 @@
 document.addEventListener("DOMContentLoaded", battrComponents.init);
 
 battrComponents.controller('AppController', function (model) {
+  model.checkboxValue = true;
+  document.body.querySelector('br-checkbox').addEventListener('change', function (value) {
+    console.log('changed', model.checkboxValue)
+  });
+
+  setTimeout(() => {
+    console.log('befoer', model.checkboxValue)
+    model.checkboxValue = !model.checkboxValue;
+    console.log('after', model.checkboxValue)
+  }, 5000);
 });
